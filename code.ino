1382b9c40e78f4e1d2f1f1a616a6d6ef4772fadf
sridel92 <sridel@free.fr>
2025-02-23 18:42:01 +0100

    Update code.ino
    sanity check

--- code.ino
+++ code.ino
@@ -26,11 +26,11 @@ programmer Arduino as ISP
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 
 const int Led1    = 0;        // PB0 (pin 5 of ATtiny85 )
-const int Led2    = 1;        // PB1 (pin 6 of ATtiny85 )
-const int Led3    = 2;        // PB2 (pin 7 of ATtiny85 ) 
-const int Led4    = 4;        // unused PB4 (pin 3 of ATtiny85 )
-const int Switch  = 3;        
-volatile int s0   = 1;
+const int Led2    = 1;       // PB1 (pin 6 of ATtiny85 )
+const int Led3    = 2;      // PB2 (pin 7 of ATtiny85 ) 
+const int Led4    = 4;     // PB4 (pin 3 of ATtiny85 )
+const int Switch  = 3;    // PB3 (pin 2 of ATtiny85    
+volatile int s0   = 1;   // 
 
 const byte unused = 4;       //
 int watchdog_counter = 0;
@@ -39,50 +39,51 @@ int LedArray[] = {Led1, Led2, Led3, Led4};
 
 // Pin change interrupt service routine
 ISR (PCINT0_vect) {
- s0++;
+ s0++;   // 1 interrupts on each press and release  : on Press button, s0 is 1 and then on release s0 is 2
  ChangeSwitch();
 }
 
 // Called when switch is pressed; On is 0 (down) or 1 (up)
 void ChangeSwitch () {
   
-switch (s0) {
+switch (s0) {  // you have an interrupt on press and another on release, so coounting case 1 + 2 for the first, case 3 + 4 for the second...
   case 1 ... 2:
     digitalWrite(LedArray[3], HIGH);
     delay(500);
     digitalWrite(LedArray[3], LOW);
-    delay(250);
+    delay(500);
     
   break;
   case 3 ... 4:
     digitalWrite(LedArray[2], HIGH);
     delay(500);
     digitalWrite(LedArray[2], LOW);
-    delay(250);
+    delay(500);
     
   break;
   case 5 ... 6:
     digitalWrite(LedArray[1], HIGH);
     delay(500);
     digitalWrite(LedArray[1], LOW);
-    delay(250);
+    delay(500);
     
   break;
   case 7 ... 8:
     digitalWrite(LedArray[0], HIGH);
     delay(500);
     digitalWrite(LedArray[0], LOW);
-    delay(250);
+    delay(500);
     
     break; 
   default:
   
-  if (s0 == 9) {
+  if (s0 == 9) { // restart the s0 at the beginning and going to first case 1 ... 2
     s0 = 0;
   }
   break;
   }
 }
+
 
 void WDDelay(int n) {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -105,35 +106,30 @@ void setup() {
   PCMSK =  1<<Switch;     // pin change mask register
   GIMSK = 1<<PCIE;       // Enable interrupt
   GIFR = 1<<PCIF;       // Clear interrupt flag
-  
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Power down everything, wake up from WDT
   sleep_enable();
-
   } // end of setup
 
 void loop() 
 {
   adc_disable(); // ADC uses ~320uA
-  //WDDelay(9);   // 8sec
-  //WDDelay(9);  // 8sec
-  //WDDelay(9); // 8sec
-
 
 switch (s0) {
-  case 1 ... 2: // ***** random 4 LEDS and 36 seconds delay *****
+  
+  case 1 ... 2: // ***** random 4 LEDS and 32 seconds delay *****
     for ( int y=0; y<4; y++ ) {            // 4 times
       int x = random(0,4);
           for ( int i=0; i<3; i++){  // 2 flashs
             digitalWrite(LedArray[x], HIGH);
             WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
             digitalWrite(LedArray[x], LOW);
-            WDDelay(3);  // 128ms
+            WDDelay(3);
             }
     }
-    WDDelay(9);
-    WDDelay(9);
-    WDDelay(9);
-    WDDelay(9);
+    WDDelay(9); // 8 sec
+    WDDelay(9); // 16 sec
+    WDDelay(9); // 24 sec
+    WDDelay(9); // 32 sec
   break;
   
   case 3 ... 4: // ***** 4 leds and 4 seconds sleep *****
@@ -142,45 +138,52 @@ switch (s0) {
         digitalWrite(LedArray[i], HIGH);
         WDDelay(0);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
         digitalWrite(LedArray[i], LOW);
-        WDDelay(2);  // 128ms
+        WDDelay(2);
        }
     }
     WDDelay(8); // 4 seconds
   break;
  
-  case 5 ... 6: // ***** 2 leds led1 and led3 - 4 seconds sleep *****
-    for ( int i=0; i<6; i++) {       // 5 flashs
-      digitalWrite(LedArray[0], HIGH);
-      WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
-      digitalWrite(LedArray[0], LOW);
-      WDDelay(2);  // 128ms
-      digitalWrite(LedArray[2], HIGH);
-      WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
-      digitalWrite(LedArray[2], LOW);
-      WDDelay(2);  // 128ms
-     }
-     WDDelay(7); // 2 seconds
+  case 5 ... 6: // *****  4 leds up and down - 4 seconds sleep *****
+    for ( int y=0; y<5; y++ ) {
+      for ( int i=0; i<4; i++) {   // Up
+        digitalWrite(LedArray[i], HIGH);
+        WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms  
+        digitalWrite(LedArray[i], LOW);
+        WDDelay(1);
+      }
+      for ( int i=3; i>=0; i--) {  // down
+        digitalWrite(LedArray[i], HIGH);
+        WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms  
+        digitalWrite(LedArray[i], LOW);
+        WDDelay(1);
+      }
+    }
+    WDDelay(8); // 4 seconds
   break;
 
-  case 7 ... 8: // ***** 1 leds (green) - 24 seconds sleep *****
-      digitalWrite(LedArray[2], HIGH);
-      WDDelay(0);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
-      digitalWrite(LedArray[2], LOW);
-      WDDelay(9);  //6=1sec, 7=2sec, 8=4sec, 9=8sec
-      WDDelay(9);
-      WDDelay(9);
-      WDDelay(9);
-      WDDelay(9);
-      WDDelay(9); // 48sec
-      WDDelay(9); // 56 sec
-      WDDelay(9); // 64 sec   
-  break;
+  case 7 ... 8: // ***** 2 + 2 leds - 4 seconds sleep *****
+  for ( int y=0; y<5; y++ ) {
+        digitalWrite(LedArray[0], HIGH);
+        digitalWrite(LedArray[2], HIGH);
+        WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms  
+        digitalWrite(LedArray[0], LOW);
+        digitalWrite(LedArray[2], LOW);
+        WDDelay(1);
+        digitalWrite(LedArray[1], HIGH);
+        digitalWrite(LedArray[3], HIGH);
+        WDDelay(1);  //0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms  
+        digitalWrite(LedArray[1], LOW);
+        digitalWrite(LedArray[3], LOW);
+        WDDelay(1);
+      }
+    WDDelay(8); // 4 seconds
+    break;
 
   default:
   break;
 }
 
-  
 } // end of loop
 
 
